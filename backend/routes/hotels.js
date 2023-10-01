9d9d7c043d62569194bed16e3efee94ec4eabe76
Krushna Nagare <contact.nagare@gmail.com>
2023-10-01 11:22:22 +0530

    Added Controller folder and made clean backend code

--- backend/routes/hotels.js
+++ backend/routes/hotels.js
@@ -1,157 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const hotelSchema = require('../model/hotelSchema');
-
-async function getHotels(req , res , next)
-{
-    let hotel;
-    try {
-        
-        hotel = await hotelSchema.findById(req.params.id); 
-        if(hotel == null)
-        {
-            return res.status(404).json({message : "The Gun you are looking fir is not found in the database"});
-        }
-
-    } catch (error) {
-        
-    }
-
-    res.hotel = hotel;
-    next()
-}
 
 
-router.get('/' ,async (req , res)=>
-{
-
-    try {
-        const result = await hotelSchema.find();
-        res.json(result);
-    } catch (error) {
-
-        res.status(500).json({message : error.message});
-        
-    }
-})
-
-
-router.get('/:id' , getHotels , (req , res)=>
-{
-
-    try {
-        res.json(res.hotel)
-        
-    } catch (error) {
-        res.status(500).json({message : error.message});
-    }
-})
+const {getallhotels , asyncFunc , getHotel , createHotel, updateHotel} = require('../controller/hotelController')
 
 
 
-// POST a new hotel
+// Get all Hotels
+router.get('/' ,getallhotels);
+
+// Get One Hotel
+router.get('/:id' , asyncFunc , getHotel);
 
 
-router.post('/' , async(req , res)=>
-{
-    const newEntry = new hotelSchema({
-        name: req.body.name,
-        city: req.body.city,
-        subtitle: req.body.subtitle,
-        information: req.body.information,
-        rating: req.body.rating,
-        reviewCount: req.body.reviewCount,
-        price: req.body.price,
-        fakePrice: req.body.fakePrice,
-        features: req.body.features,
-        mainImage: req.body.mainImage,
-        image2: req.body.image2,
-        image3: req.body.image3,
-        images: req.body.images,
-        link: req.body.link,       
-        catogary: req.body.catogary  
-    })
-
-    try {
-
-        const postEntry = await newEntry.save();
-        res.json(postEntry);
-        
-    } catch (error) {
-        res.status(500).json({message : error.message});
-
-    }
-})
+// Create a new hotel
+router.post('/' , createHotel)
 
 
-
-router.patch('/:id' , getHotels,async(req,res)=>{
-
-
-    if (req.body.name != null) {
-        res.hotel.name = req.body.name;
-    }
-    if (req.body.city != null) {
-        res.hotel.city = req.body.city;
-    }
-    if (req.body.subtitle != null) {
-        res.hotel.subtitle = req.body.subtitle;
-    }
-    if (req.body.information != null) {
-        res.hotel.information = req.body.information;
-    }
-    if (req.body.rating != null) {
-        res.hotel.rating = req.body.rating;
-    }
-    if (req.body.reviewCount != null) {
-        res.hotel.reviewCount = req.body.reviewCount;
-    }
-    if (req.body.price != null) {
-        res.hotel.price = req.body.price;
-    }
-    if (req.body.fakePrice != null) {
-        res.hotel.fakePrice = req.body.fakePrice;
-    }
-    if (req.body.features != null) {
-        res.hotel.features = req.body.features;
-    }
-    if (req.body.mainImage != null) {
-        res.hotel.mainImage = req.body.mainImage;
-    }
-    if (req.body.image2 != null) {
-        res.hotel.image2 = req.body.image2;
-    }
-    if (req.body.image3 != null) {
-        res.hotel.image3 = req.body.image3;
-    }
-    if (req.body.images != null) {
-        res.hotel.images = req.body.images;
-    }
-    if (req.body.catogary != null) {
-        res.hotel.catogary = req.body.catogary;
-    }
-    if (req.body.link != null) {
-        res.hotel.link = req.body.link;
-    }
-    
-
-
-    try 
-    {
-        const patchEntry = await res.hotel.save();
-        res.json(patchEntry);
-        
-
-        
-    } catch (error) {
-        res.status(400).json({message : error.message});
-      
-    }
-
-
-
-})
+// Update a Hotel
+router.patch('/:id' , asyncFunc,updateHotel)
 
 
 module.exports = router;
-
